f6248ac8ce17bd1a2dbc04605e274677ed2dfa3c
Piyush Mehta <me@piyushmehta.com>
2021-07-09 15:19:05 +0530

    Sidebar

--- src/utils/navbarMenu.ts
+++ src/utils/navbarMenu.ts
@@ -1,3 +1,7 @@
+import { CgProfile } from "react-icons/cg";
 import { MdHome } from "react-icons/md";
-const NavbarMenu = [{ key: 1, name: "Home", url: "/home", icon: MdHome }];
+const NavbarMenu = [
+  { key: 1, name: "Home", url: "/home", icon: MdHome },
+  { key: 2, name: "My Profile", url: "/profile", icon: CgProfile },
+];
 export default NavbarMenu;
