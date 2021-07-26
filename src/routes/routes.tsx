7d9fd83f73aa7e04bc2c351ba3e619f5cbe403e3
Piyush Mehta <me@piyushmehta.com>
2021-07-26 16:13:23 +0530

    fixed Navbar

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,13 @@
-import { LocationProvider } from "@reach/router";
 import React from "react";
 import ProtectedRoutes from "./protected.routes";
 import PublicRoutes from "./public.routes";
 
 const Routes: React.FC = () => {
   return (
-    <LocationProvider>
+    <>
       <PublicRoutes />
       <ProtectedRoutes />
-    </LocationProvider>
+    </>
   );
 };
 
--- src/tests/components/Navbar/index.test.tsx
+++ src/tests/components/Navbar/index.test.tsx
@@ -8,43 +8,38 @@ afterEach(cleanup);
 describe("Navbar", () => {
   it("Navbar Content Not Logged In and is Healthy", () => {
     const { getByText, getByTestId } = render(
-      <AuthProvider testUser={null}>
-        <Navbar />
-      </AuthProvider>
-    );
-    /** @type {HTMLElement} */
-    let navbarMenu: HTMLElement[] = [];
-    // Navbar Menu currently contains these three items
-    expect(getByText("Home")).toBeTruthy();
-    navbarMenu.push(getByText("Home"));
-    expect(getByText("Login")).toBeTruthy();
-    navbarMenu.push(getByText("Login"));
-    expect(getByText("Sign Up")).toBeTruthy();
-    navbarMenu.push(getByText("Sign Up"));
-
-    // Title is the app name
-    expect(getByText(APP_NAME)).toBeTruthy();
-    // Links should be 3
-    expect(navbarMenu.length).toBe(3);
-    // and a Theme switch
-    expect(getByTestId("theme-switch")).toBeTruthy();
-  });
-  it("Navbar Content  Logged In and is Healthy", async () => {
-    localStorage.setItem("isAuth", "true");
-    const { findAllByText } = render(
       <AuthProvider>
         <Navbar />
       </AuthProvider>
     );
-    expect(await findAllByText("Home")).toBeFalsy();
-    // Navbar Menu currently contains None and Navbar will not render if logged in
-    // expect(findAllByText("Home")).toBeFalsy();
-    // expect(findByText("Login")).toBeFalsy();
-    // expect(findByText("Sign Up")).toBeFalsy();
 
-    // // Title is the app name
-    // expect(findByText(APP_NAME)).toBeFalsy();
-    // // and a Theme switch
-    // expect(findByTestId("theme-switch")).toBeFalsy();
+    // Navbar Menu currently contains these three items
+    expect(getByText("Home")).toBeTruthy();
+    expect(getByText("Login")).toBeTruthy();
+    expect(getByText("Sign Up")).toBeTruthy();
+
+    // Title is the app name
+    expect(getByText(APP_NAME)).toBeTruthy();
+
+    // and a Theme switch
+    expect(getByTestId("theme-switch")).toBeTruthy();
+  });
+
+  it("Navbar Content If user Logged In and is on HomePage", async () => {
+    localStorage.setItem("isAuth", "true");
+    const { getByText, findByTestId } = render(
+      <AuthProvider>
+        <Navbar />
+      </AuthProvider>
+    );
+
+    // Navbar Menu currently contains None and Navbar will only render SignOut and Theme Button
+    /** @type {HTMLElement} */
+    let navbarMenu: HTMLElement[] = [];
+    expect(getByText("Sign Out")).toBeTruthy();
+    navbarMenu.push(getByText("Sign Out"));
+
+    // and a Theme switch
+    expect(await findByTestId("theme-switch")).toBeTruthy();
   });
 });
