a048b56cb88e10e941efb482756c67b5f35eb0f9
Piyush Mehta <me@piyushmehta.com>
2021-07-27 14:07:24 +0530

    test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const URL = "http://localhost:3000";
 const KEY = "some.fake.key";
 
-const supabase = createClient(URL, KEY);
+createClient(URL, KEY);
 
 test("Build to succeed", async () => {
   // Basic test to ensure TS build is working.
